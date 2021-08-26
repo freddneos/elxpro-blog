2bd42b55b89fd5035347af5dec814c37e992a5f1
freddneos <fredd@neosdev.com.br>
2021-08-27 00:23:40 +0100

    fix codecov error

--- mix.exs
+++ mix.exs
@@ -16,7 +16,8 @@ defmodule Blog.MixProject do
         coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
-        "coveralls.html": :test
+        "coveralls.html": :test,
+        "coveralls.html": :json
       ]
     ]
   end
